32d5ca582514c6c8fc88aae018b2779d50abba4b
Elizabeth Lynch <liz@plsys.co.uk>
2015-05-07 20:08:18 +0100

    Changed soe-loader.sh script to access the new Daybreak census.daybreakgames.com instead of data.soe.com.

--- soe-loader/soe-loader.sh
+++ soe-loader/soe-loader.sh
@@ -6,6 +6,6 @@ databasedir=/Volumes/Macintosh_HD_2/Projects/Clojure/soe-loader/
 database=/Volumes/Macintosh_HD_2/Projects/Clojure/soe-loader/soe_status.sq3
 command="/usr/bin/java -jar /Volumes/Macintosh_HD_2/Projects/Clojure/soe-loader/target/soe-loader-0.1.0-SNAPSHOT-standalone.jar"
 
-/usr/bin/curl -silent --no-include --output ${outfile} http://data.soe.com/json/status/
+/usr/bin/curl -silent --no-include --output ${outfile} http://census.daybreakgames.com/json/status/
 cd ${databasedir}
 ${command} ${outfile} ${datetext}
